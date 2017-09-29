ab55a53f08d5a200bb60523534304a82cba0a06c
Choi Kirin <iizs@naver.com>
2017-09-30 01:58:26 +0900

    Add Referer header

--- api/v1/views.py
+++ api/v1/views.py
@@ -16,7 +16,8 @@ class ItemNotFoundException(Exception):
 
 def _make_request(url):
     request = urllib2.Request(url)
-    request.add_header('User-Agent', 'LegoSrchBot/1.0 +http://iizs.net/legosrch')
+    #request.add_header('User-Agent', 'LegoSrchBot/1.0 +http://iizs.net/legosrch')
+    request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
     return request
 
 # TODO remove
@@ -106,10 +107,16 @@ def _get_from_wikia(opener, num):
 
 def _get_from_lego_dot_com(opener, num):
     request = _make_request('https://sp1004f1fe.guided.ss-omtrdc.net/?do=json-db&callback=json&count=18&q={query}&cc=us&lang=en&jsonp=jsonCallback'.format(query=num))
+
+    # If Referer omitted, result does not comes out.
+    request.add_header('Referer', 'https://search2.lego.com/en-US/{query}'.format(query=num))
+
     body = opener.open(request).read()
+
     # removes "jsonCallback( .. )"
     # request url에서 jsonp=jsonCallback query parameter를 제거해도 같은 효과이지만, 웹 페이지에서 부르는 것와 패턴을 맞추기 위해서 살려둠
     body = re.sub( ' \)', '', re.sub( 'jsonCallback\(', '', body), re.M )
+
     srch_result = json.loads(body)
     resp = []
     for i in srch_result['results']:
@@ -126,7 +133,7 @@ def _get_from_lego_dot_com(opener, num):
                 s['sku_number'] = l['sku_number']
                 s['price'] = l['list_price']   # can be null
                 s['list_price_formatted'] = l['list_price_formatted']
-                s['shop_url'] = 'https://shop.lego.com/en-US/{seo_id}'.format(seo_id=d['id'])
+                s['shop_url'] = 'https://shop.lego.com/en-US/{seo_id}'.format(seo_id=d['product_id'])
                 d['skus'].append(s)
             resp.append(d)
     return resp
